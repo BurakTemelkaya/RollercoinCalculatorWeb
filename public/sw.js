050a5588198c76ca7f90082587a81f2fc9efd393
Burak Temelkaya <temelkayaburak@gmail.com>
2026-05-06 06:00:44 +0300

    refactor: remove trailing-slash URL redirection script from index.html

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Update this version whenever CURRENT_CACHE_VERSION changes in App.tsx
-const CACHE_VERSION = '20260506.044950';
+const CACHE_VERSION = '20260506.060044';
 const CACHE_NAME = `rollercoin-${CACHE_VERSION}`;
 
 self.addEventListener('install', () => {
