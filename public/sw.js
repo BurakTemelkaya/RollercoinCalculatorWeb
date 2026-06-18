50a217d44a57e79eb77eeb74d9ea77a91ed3f56e
Burak Temelkaya <temelkayaburak@gmail.com>
2026-06-18 03:15:02 +0300

    feat: disable Cloudflare rocket loader for Coinzilla ad scripts

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Update this version whenever CURRENT_CACHE_VERSION changes in App.tsx
-const CACHE_VERSION = '20260618.030736';
+const CACHE_VERSION = '20260618.031502';
 const CACHE_NAME = `rollercoin-${CACHE_VERSION}`;
 
 self.addEventListener('install', () => {
